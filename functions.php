2d84486d4a05cb4dd1954ddab3beaf1a72ced62a
johncook1979 <41190295+johncook1979@users.noreply.github.com>
2023-03-18 16:37:00 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,11 +5,31 @@
   *
 */
 
+// Sitewide
+add_filter( 'woocommerce_product_get_price', 'my_increase_price', 10, 2 );
 
-add_filter( 'woocommerce_product_get_price', 'wc_success_academy_increase_price', 10, 2 );
-
-function wc_success_academy_increase_price( $price ){
+function my_increase_price( $price ){
 
   return $price + 6; // + 6 is the value to increase the price by. To decrease by 6 use - 6
 
 }
+
+
+/**
+ *
+ * you can further enhance the code above to apply it by category
+ * Interchange product_cat and product_tag for categories and tags. Use the OR (||) operator to apply it to multiple terms
+ *
+*/
+
+// Only to specific terms
+add_filter( 'woocommerce_product_get_price', 'my_increase_price_terms', 10, 2 );
+
+function my_increase_price_terms( $price ){
+
+  if ( has_term( 'shirts', 'product_cat' ) ) {
+    return $price + 6; // + 6 is the value to increase the price by. To decrease by 6 use - 6
+  }
+}
+
+
